d13f783457366d2941ec9307a2fa4a6dd30b568c
Taha Jalil <taha.jalil@gmail.com>
2014-03-27 17:02:48 -0400

    people.rb complete

--- people.rb
+++ people.rb
@@ -1,19 +1,52 @@
 class Person
+  
   attr_accessor :name
 
-  def initializer(name)
+  def initialize(name)
     @name = name
   end
+
+  def greeting
+    puts "Hi my name is #{@name}"
+    puts self
+  end
+
 end
 
-class Student
+class Student < Person
+
+# did not initialize again because the instance variable @name has already been initialized in the parent class Person
+# if Instructor had another instance variable, we would have had to initialize @name (super(name)) along with the new instance variable
+
   def learn
     "I get it!"
   end
+
 end
 
-class Instructor
+class Instructor < Person
+
+# did not initialize again because the instance variable @name has already been initialized in the parent class Person
+# if Instructor had another instance variable, we would have had to initialize @name (super(name)) along with the new instance variable
+
   def teach
     "Everything in Ruby is an Object"
   end
+
 end
+
+student = Student.new("Christina")
+instructor = Instructor.new("Chris")
+
+puts student.greeting
+puts instructor.greeting
+
+puts student.learn
+puts instructor.teach
+
+puts student.teach
+# throws an undefined method error (NoMethodError) because 'teach' is a instance method 
+# that is only available to the instance of Instructor. Had teach been created in Person 
+# or Student, we would have been able to call it on our instance of Student
+
+
