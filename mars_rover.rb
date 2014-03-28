bdd116c82cd0ca4758718d6e953a21efbe87c0c7
Taha Jalil <taha.jalil@gmail.com>
2014-03-28 19:05:44 -0400

    improved Mars_Rover, controlled by user input, asks for another Rover

--- mars_rover.rb
+++ mars_rover.rb
@@ -7,20 +7,21 @@ class Grid
   end
 
   def size
-    "The size of the grid: #{@x} #{@y}"
+    puts "The size of the grid: #{@x} #{@y}"
   end
 end
 
-class Rover < Grid
+class Rover
   attr_accessor :direction
 
   def initialize(x,y,direction)
-    super(x,y)
+    @x = x
+    @y = y
     @direction = direction
   end
 
   def state
-    "Current location of Rover: #{@x} #{@y} #{@direction}"
+    puts "Current location of Rover: #{@x} #{@y} #{@direction}"
   end
 
   def go_left
@@ -72,22 +73,44 @@ class Rover < Grid
         go_straight
       end
     end
+    state
   end
-
 end #end class Rover
 
-#create a 5 by 5 grid
-grid = Grid.new(5,5)
-puts grid.size
+class Operate
 
-#create rover 
-rover1 = Rover.new(1, 2, "N")
-puts rover1.state
+    def create
+      puts "Exploring Mars with Taha's Rover!".upcase
+      puts "Input the size of the grid like such: X Y"
+      grid_size = gets.chomp.split(" ")
+      grid_x = grid_size[0].to_i
+      grid_y = grid_size[1].to_i
+      my_grid = Grid.new(grid_x, grid_y)
+      my_grid.size
 
-#move rover
-rover1.move
+      puts "Input the starting coordinates of Taha's Rover as such: X Y direction"
+      initial_state = gets.chomp.split(" ")
+      rover_x = initial_state[0].to_i
+      rover_y = initial_state[1].to_i
+      rover_direction = initial_state[2]
+      my_rover = Rover.new(rover_x, rover_y, rover_direction)
 
-#show rover's final position and direction
-puts rover1.state
+      my_rover.move
 
+      done = false
+      while !done
+      puts "Would you like to create another Rover? Type 'Y' or 'N'"
+      another_rover = gets.chomp
+        if another_rover == 'Y'
+          create
+        elsif another_rover == 'N'
+          done = true
+        end
+      end
+    end
 
+end
+
+#call the create method
+operate = Operate.new
+operate.create
