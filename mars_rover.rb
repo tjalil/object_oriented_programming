f3ed010cbd23b7a25adcc2fa658873d66385ceb3
Taha Jalil <taha.jalil@gmail.com>
2014-03-27 23:45:39 -0400

    completed method grid

--- mars_rover.rb
+++ mars_rover.rb
@@ -17,22 +17,100 @@ class Rover
     @y = y
     @direction = direction
   end
+end
 
-  def grid_size
+  def grid
+    puts "Grid size needed. Pass it as such: x y"
+    grid_size = gets.chomp
 
+    grid_array = []
+    grid_array = grid_size.split(" ").to_a
+
+    grid_array.map! do |num|
+      num.to_i
+    end
+
+    grid_x = grid_array[0]
+    grid_y = grid_array[1]
+
+    puts "Your grid is #{grid_x} by #{grid_y}"
   end
 
   def initial_state
-    puts "For the initial state of Rover we require, x, y, and direction. Pass them as such: x y direction"
-    
+    puts "For the initial state of Rover we require, x, y, and direction(N,S,E,W). Pass them as such: x y direction"
+    initial = gets.chomp
+
+    initial_array = []
+    initial_array = initial.split(" ").to_a
+    initial_direction = initial_array[-1]
+
+    #need to separate coordinates and direction OR assign number to direction so that to_i can be run on entire array
+
+    initial_array.map! do |num|
+      num.to_i
+    end
+
+    initial_x = intial_array[0]
+    initial_y = intial_array[1]
+    initial_direction = intial_array[2]
 
   end
 
+  initial_state
+
   def motion
+    puts "Input the set of directions you want the Rover to follow"
+    rover_motion = gets.chomp
+
+    motion_array = []
+    motion_array = rover_motion.split("").to_a
+
+    motion_array.each do |direction|
+      #if initial_direction = N and motion_array = L 
+        #new_direction = W
+      
+      #if initial_direction = S and motion_array = L
+        #new_direction = E
+      
+      #if initial_direction = E and motion_array = L 
+        #new_direction = N
+
+      #if initial_direction = W and motion_array = L 
+        #new_direction = S
+
+      
+
+      #if initial_direction = N and motion_array = R 
+        #new_direction = E
+
+      #if initial_direction = S and motion_array = R 
+        #new_direction = W
+
+      #if initial_direction = E and motion_array = R 
+        #new_direction = S
+
+      #if initial_direction = W and motion_array = R 
+        #new_direction = N
+
+
+
+
+      #if initial_direction = N and motion_array = M 
+        #new_direction = N && new_y += 1
+
+      #if initial_direction = S and motion_array = M 
+        #new_direction = S && new_y -= 1
+
+      #if initial_direction = E and motion_array = M 
+        #new_direction = E && new_x += 1
+
+      #if initial_direction = W and motion_array = M 
+        #new_direction = W && new_x -= 1
+
+    end
+
   end
 
   # def new_rover
   # end
-
-end
 
